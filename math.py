bfacca5136e0a1c5bd87bce6ba5d0a7101089c82
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2022-02-07 22:24:12 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implementation
+def add(x,y):
+return x+y
+
+#Substract implementation
+def subtract(x,y):
+return x*y
+
+#multiply implementation
+def multiply(x,y):
+return x*y
+
+#divide implementation
+def divide(x,y):
+
+if(y==0)
+return DIVIDE_BY_0
+else 
+return x/y          #on Bug789 branch
+
+def square(x):
+return x*x
